7f5ec8de9d8d34520a3621e76022e5b75e92eaa1
Kyle Fidalgo <fidalgok@MDKLFLL378LAWL.local>
2020-05-22 11:26:29 -0400

    Initial project setup and created a fretboard object to keep track of guitar notes

---
+++ data/notes.js
@@ -0,0 +1,174 @@
+export const fretBoard = {
+    'E String': {
+        open: {
+            note: 'E',
+            sound: '',
+        },
+        '1st Fret': {
+            note: 'F',
+            sound: '',
+        },
+        '2nd Fret': {
+            note: 'F#/G♭',
+            sound: '',
+        },
+        '3rd Fret': {
+            note: 'G',
+            sound: '',
+        },
+        '4th Fret': {
+            note: 'G#/A♭',
+            sound: '',
+        },
+        '5th Fret': {
+            note: 'A',
+            sound: '',
+        },
+        '6th Fret': {
+            note: 'A#/B♭',
+            sound: '',
+        },
+        '7th Fret': {
+            note: 'B',
+            sound: '',
+        },
+        '8th Fret': {
+            note: 'C',
+            sound: '',
+        },
+        '9th Fret': {
+            note: 'C#/D♭',
+            sound: '',
+        },
+        '10th Fret': {
+            note: 'D',
+            sound: '',
+        },
+        '11th Fret': {
+            note: 'D#/E♭',
+            sound: '',
+        },
+        '12th Fret': {
+            note: 'E',
+            sound: '',
+        },
+        '13th Fret': {
+            note: 'F',
+            sound: '',
+        },
+        '14th Fret': {
+            note: 'F#/G♭',
+            sound: '',
+        },
+        '15th Fret': {
+            note: 'G',
+            sound: '',
+        },
+        '16th Fret': {
+            note: 'G#/A♭',
+            sound: '',
+        },
+        '17th Fret': {
+            note: 'A',
+            sound: '',
+        },
+        '18th Fret': {
+            note: 'A#/B♭',
+            sound: '',
+        },
+        '19th Fret': {
+            note: 'B',
+            sound: '',
+        },
+        '20th Fret': {
+            note: 'C',
+            sound: '',
+        },
+    },
+    'A String': {
+        open: {
+            note: 'A',
+            sound: '',
+        },
+        '1st Fret': {
+            note: 'A#/B♭',
+            sound: '',
+        },
+        '2nd Fret': {
+            note: 'B',
+            sound: '',
+        },
+        '3rd Fret': {
+            note: 'C',
+            sound: '',
+        },
+        '4th Fret': {
+            note: 'C#/D♭',
+            sound: '',
+        },
+        '5th Fret': {
+            note: 'D',
+            sound: '',
+        },
+        '6th Fret': {
+            note: 'D#/E♭',
+            sound: '',
+        },
+        '7th Fret': {
+            note: 'E',
+            sound: '',
+        },
+        '8th Fret': {
+            note: 'F',
+            sound: '',
+        },
+        '9th Fret': {
+            note: 'F#/G♭',
+            sound: '',
+        },
+        '10th Fret': {
+            note: 'G',
+            sound: '',
+        },
+        '11th Fret': {
+            note: 'G#/A♭',
+            sound: '',
+        },
+        '12th Fret': {
+            note: 'A',
+            sound: '',
+        },
+        '13th Fret': {
+            note: 'A#/B♭',
+            sound: '',
+        },
+        '14th Fret': {
+            note: 'B',
+            sound: '',
+        },
+        '15th Fret': {
+            note: 'C',
+            sound: '',
+        },
+        '16th Fret': {
+            note: 'C#/D♭',
+            sound: '',
+        },
+        '17th Fret': {
+            note: 'D',
+            sound: '',
+        },
+        '18th Fret': {
+            note: 'D#/E♭',
+            sound: '',
+        },
+        '19th Fret': {
+            note: 'E',
+            sound: '',
+        },
+        '20th Fret': {
+            note: 'F',
+            sound: '',
+        },
+    }
+}
